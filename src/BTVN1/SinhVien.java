8b1fbe1b2a0a6f3b6ea3db309d68f764de284971
tandv1 <131516144+tandv1@users.noreply.github.com>
2023-04-26 16:06:50 +0700

    Update SinhVien.java

--- src/BTVN1/SinhVien.java
+++ src/BTVN1/SinhVien.java
@@ -3,17 +3,15 @@ package BTVN1;
 import java.net.IDN;
 
 public class SinhVien {
-    static int ID=3645221;
     static String Lop= "Điện - Điện tử";
     public static void main(String[] args) {
-
         String name = "Đỗ Văn Tân";
         System.out.println("Họ tên SV: " + name);
         MSV();
         Lop();
     }
     public static void MSV (){
-        System.out.println("Mã SV: " + ID);
+        System.out.println("Mã SV: " + Thontin.ID);
     }
     public static void Lop (){
         System.out.println("Lớp: "+ Lop);
