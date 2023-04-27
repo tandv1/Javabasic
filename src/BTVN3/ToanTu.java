0644c95561f88d2161091dab21f38beec906c964
tandv1 <dv.tanta@gmail.com>
2023-04-27 21:55:38 +0700

    Commit BT3

---
+++ src/BTVN3/ToanTu.java
@@ -0,0 +1,21 @@
+package BTVN3;
+
+public class ToanTu {
+    public static int number = 100;
+
+    static String check(int n) {
+        if (n == number) {
+            return "Tham số n bằng với số number";
+        } else if (n < number) {
+            return "Tham số n nhỏ hơn số number";
+        } else if (n > number) {
+            return "Tham số n lớn hơn số number";
+        }
+        return "Số n rỗng";
+    }
+
+    public static void main(String[] args) {
+        String ketqua = check(100);
+        System.out.println(ketqua);
+    }
+}
